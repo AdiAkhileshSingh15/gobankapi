06354a9257807c9f552ee98043f99f693c116333
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-06-26 00:59:22 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", store)
+	// server := NewAPIServer(":8000", store)
+	// server.Run()
+}
